47b8b09658e2f9b140d8e8c9626a2e27a4bf0685
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-05 13:35:28 +0900

    新增BarcodeScanActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
 }
---
+++ app/src/main/java/com/example/inventoryapp/BarcodeScanActivity.kt
@@ -0,0 +1,20 @@
+package com.example.inventoryapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BarcodeScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_barcode_scan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
